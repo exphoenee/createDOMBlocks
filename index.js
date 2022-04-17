d20b7c5583d3fcfc812c628350dda977d7d8da71
exphoenee <bozzay.viktor@gmail.com>
2022-04-17 21:40:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createInput({
         parent,
         attrs: {
           for: id,
-          class: `input-container${classes ? ` ${classes}` : ""}`,
+          class: `input-label${classes ? ` ${classes}` : ""}`,
         },
       },
       {
@@ -34,6 +34,98 @@ function createInput({
           class: `input-container${classes ? ` ${classes}` : ""}`,
           type,
           placeholder,
+          value,
+        },
+        handleEvent: [
+          onChange && { event: "change", cb: onChange },
+          click && { event: "click", cb: click },
+          ...(handleEvent && makeThatArray(handleEvent)),
+        ],
+      },
+    ],
+  });
+}
+function createSelect({
+  parent,
+  classes,
+  id,
+  name,
+  labelText,
+  value,
+  onChange,
+  click,
+  handleEvent,
+  options,
+}) {
+  createDOMElem({
+    tag: div,
+    parent,
+    attrs: { class: `select-container${classes ? ` ${classes}` : ""}` },
+    children: [
+      {
+        tag: label,
+        text: labelText,
+        parent,
+        attrs: {
+          for: id,
+          class: `input-label${classes ? ` ${classes}` : ""}`,
+        },
+      },
+      {
+        tag: select,
+        parent,
+        attrs: {
+          id,
+          name: name || id,
+          class: `input-container${classes ? ` ${classes}` : ""}`,
+          value,
+        },
+        handleEvent: [
+          onChange && { event: "change", cb: onChange },
+          click && { event: "click", cb: click },
+          ...(handleEvent && makeThatArray(handleEvent)),
+        ],
+        children: options.map(({ text, value }) => {
+          return { tag: option, text, attrs: { value } };
+        }),
+      },
+    ],
+  });
+}
+
+function createTextarea({
+  parent,
+  classes,
+  id,
+  name,
+  labelText,
+  value,
+  onChange,
+  click,
+  handleEvent,
+  options,
+}) {
+  createDOMElem({
+    tag: div,
+    parent,
+    attrs: { class: `textarea-container${classes ? ` ${classes}` : ""}` },
+    children: [
+      {
+        tag: label,
+        text: labelText,
+        parent,
+        attrs: {
+          for: id,
+          class: `input-label${classes ? ` ${classes}` : ""}`,
+        },
+      },
+      {
+        tag: textarea,
+        parent,
+        attrs: {
+          id,
+          name: name || id,
+          class: `input-container${classes ? ` ${classes}` : ""}`,
           value,
         },
         handleEvent: [
