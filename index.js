b08435625eb34aa22fd4c220543c9dee6f934d18
exphoenee <bozzay.viktor@gmail.com>
2022-04-17 22:07:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function createInput({
   parent,
+  labelfirst = true,
   classes,
   id,
   name,
@@ -11,42 +12,44 @@ function createInput({
   click,
   handleEvent,
 }) {
+  const children = [
+    {
+      tag: label,
+      text: labelText,
+      parent,
+      attrs: {
+        for: id,
+        class: `input-label${classes ? ` ${classes}` : ""}`,
+      },
+    },
+    {
+      tag: input,
+      parent,
+      attrs: {
+        id,
+        name: name || id,
+        class: `${type}-input${classes ? ` ${classes}` : ""}`,
+        type,
+        placeholder,
+        value,
+      },
+      handleEvent: [
+        onChange && { event: "change", cb: onChange },
+        click && { event: "click", cb: click },
+        ...(handleEvent && makeThatArray(handleEvent)),
+      ],
+    },
+  ];
   createDOMElem({
     tag: div,
     parent,
-    attrs: { class: `input-container${classes ? ` ${classes}` : ""}` },
-    children: [
-      {
-        tag: label,
-        text: labelText,
-        parent,
-        attrs: {
-          for: id,
-          class: `input-label${classes ? ` ${classes}` : ""}`,
-        },
-      },
-      {
-        tag: input,
-        parent,
-        attrs: {
-          id,
-          name: name || id,
-          class: `input-container${classes ? ` ${classes}` : ""}`,
-          type,
-          placeholder,
-          value,
-        },
-        handleEvent: [
-          onChange && { event: "change", cb: onChange },
-          click && { event: "click", cb: click },
-          ...(handleEvent && makeThatArray(handleEvent)),
-        ],
-      },
-    ],
+    attrs: { class: `${type}-input${classes ? ` ${classes}` : ""}` },
+    children: labelfirst ? children : children.reverse(),
   });
 }
 function createSelect({
   parent,
+  labelfirst = true,
   classes,
   id,
   name,
@@ -57,43 +60,93 @@ function createSelect({
   handleEvent,
   options,
 }) {
+  const children = [
+    {
+      tag: label,
+      text: labelText,
+      parent,
+      attrs: {
+        for: id,
+        class: `input-label${classes ? ` ${classes}` : ""}`,
+      },
+    },
+    {
+      tag: select,
+      parent,
+      attrs: {
+        id,
+        name: name || id,
+        class: `select-input${classes ? ` ${classes}` : ""}`,
+        value,
+      },
+      handleEvent: [
+        onChange && { event: "change", cb: onChange },
+        click && { event: "click", cb: click },
+        ...(handleEvent && makeThatArray(handleEvent)),
+      ],
+      children: options.map(({ text, value }) => {
+        return { tag: option, text, attrs: { value } };
+      }),
+    },
+  ];
   createDOMElem({
     tag: div,
     parent,
     attrs: { class: `select-container${classes ? ` ${classes}` : ""}` },
-    children: [
-      {
-        tag: label,
-        text: labelText,
-        parent,
-        attrs: {
-          for: id,
-          class: `input-label${classes ? ` ${classes}` : ""}`,
-        },
+    children: labelfirst ? children : children.reverse(),
+  });
+}
+function createCheckbox({
+  parent,
+  labelfirst = true,
+  classes,
+  id,
+  name,
+  labelText,
+  value,
+  onChange,
+  click,
+  handleEvent,
+  checked,
+}) {
+  const children = [
+    {
+      tag: label,
+      text: labelText,
+      parent,
+      attrs: {
+        for: id,
+        class: `input-label${classes ? ` ${classes}` : ""}`,
       },
-      {
-        tag: select,
-        parent,
-        attrs: {
-          id,
-          name: name || id,
-          class: `input-container${classes ? ` ${classes}` : ""}`,
-          value,
-        },
-        handleEvent: [
-          onChange && { event: "change", cb: onChange },
-          click && { event: "click", cb: click },
-          ...(handleEvent && makeThatArray(handleEvent)),
-        ],
-        children: options.map(({ text, value }) => {
-          return { tag: option, text, attrs: { value } };
-        }),
+    },
+    {
+      tag: input,
+      parent,
+      attrs: {
+        id,
+        name: name || id,
+        type: "checkbox",
+        class: `$checkbox-input${classes ? ` ${classes}` : ""}`,
+        value,
+        checked,
       },
-    ],
+      handleEvent: [
+        onChange && { event: "change", cb: onChange },
+        click && { event: "click", cb: click },
+        ...(handleEvent && makeThatArray(handleEvent)),
+      ],
+    },
+  ];
+  createDOMElem({
+    tag: div,
+    parent,
+    attrs: { class: `checkbox-container${classes ? ` ${classes}` : ""}` },
+    children: labelfirst ? children : children.reverse(),
   });
 }
 function createTextarea({
   parent,
+  labelfirst = true,
   classes,
   id,
   name,
@@ -106,38 +159,39 @@ function createTextarea({
   click,
   handleEvent,
 }) {
+  const children = [
+    {
+      tag: label,
+      text: labelText,
+      parent,
+      attrs: {
+        for: id,
+        class: `input-label${classes ? ` ${classes}` : ""}`,
+      },
+    },
+    {
+      tag: textarea,
+      parent,
+      text: value,
+      attrs: {
+        id,
+        rows,
+        cols,
+        placeholder,
+        name: name || id,
+        class: `textarea-input${classes ? ` ${classes}` : ""}`,
+      },
+      handleEvent: [
+        onChange && { event: "change", cb: onChange },
+        click && { event: "click", cb: click },
+        ...(handleEvent && makeThatArray(handleEvent)),
+      ],
+    },
+  ];
   createDOMElem({
     tag: div,
     parent,
     attrs: { class: `textarea-container${classes ? ` ${classes}` : ""}` },
-    children: [
-      {
-        tag: label,
-        text: labelText,
-        parent,
-        attrs: {
-          for: id,
-          class: `input-label${classes ? ` ${classes}` : ""}`,
-        },
-      },
-      {
-        tag: textarea,
-        parent,
-        text: value,
-        attrs: {
-          id,
-          rows,
-          cols,
-          placeholder,
-          name: name || id,
-          class: `input-container${classes ? ` ${classes}` : ""}`,
-        },
-        handleEvent: [
-          onChange && { event: "change", cb: onChange },
-          click && { event: "click", cb: click },
-          ...(handleEvent && makeThatArray(handleEvent)),
-        ],
-      },
-    ],
+    children: labelfirst ? children : children.reverse(),
   });
 }
