6cadaf68c7defb2379159b3811d1dc0dd8aeac2f
exphoenee <bozzay.viktor@gmail.com>
2022-04-18 14:26:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,74 +55,7 @@ function createInputContainer(params, children) {
 }
 
 /* DOM creator functions */
-function createCheckbox({
-  parent,
-  labelfirst = true,
-  classes,
-  id,
-  type = "checkbox",
-  name,
-  labelText,
-  value,
-  onChange,
-  click,
-  handleEvent,
-  checked,
-}) {
-  createDOMElem({
-    tag: div,
-    parent,
-    attrs: { class: `${type}-container${classes ? ` ${classes}` : ""}` },
-    children: createChildren(input, {
-      parent,
-      labelfirst,
-      classes,
-      id,
-      name,
-      labelText,
-      type,
-      value,
-      onChange,
-      click,
-      handleEvent,
-      checked,
-    }),
-  });
-}
-function createCheckbox2({
-  parent,
-  labelfirst = true,
-  classes,
-  id,
-  type = "checkbox",
-  name,
-  labelText,
-  value,
-  onChange,
-  click,
-  handleEvent,
-  checked,
-}) {
-  createDOMElem({
-    tag: div,
-    parent,
-    attrs: { class: `${type}-container${classes ? ` ${classes}` : ""}` },
-    children: createChildren(input, {
-      parent,
-      labelfirst,
-      classes,
-      id,
-      name,
-      labelText,
-      type,
-      value,
-      onChange,
-      click,
-      handleEvent,
-      checked,
-    }),
-  });
-}
+
 function createInput({
   parent,
   labelfirst = true,
@@ -137,24 +70,24 @@ function createInput({
   click,
   handleEvent,
 }) {
-  createDOMElem({
-    tag: div,
+  const params = {
     parent,
-    attrs: { class: `${type}-input${classes ? ` ${classes}` : ""}` },
-    children: createChildren(input, {
-      labelfirst,
-      classes,
-      id,
-      name,
-      type,
-      labelText,
-      value,
-      placeholder,
-      onChange,
-      click,
-      handleEvent,
-    }),
-  });
+    labelfirst,
+    classes,
+    id,
+    name,
+    type,
+    labelText,
+    value,
+    placeholder,
+    onChange,
+    click,
+    handleEvent,
+  };
+  createDOMElem(createInputContainer(params, createChildren(input, params)));
+}
+function createCheckbox(params) {
+  createInput({ ...params, type: "checkbox" });
 }
 function createSelect({
   parent,
@@ -169,35 +102,6 @@ function createSelect({
   handleEvent,
   options,
 }) {
-  const children = [
-    {
-      tag: label,
-      text: labelText,
-      parent,
-      attrs: {
-        for: id,
-        class: `input-label${classes ? ` ${classes}` : ""}`,
-      },
-    },
-    {
-      tag: select,
-      parent,
-      attrs: {
-        id,
-        name: name || id,
-        class: `select-input${classes ? ` ${classes}` : ""}`,
-        value,
-      },
-      handleEvent: [
-        onChange && { event: "change", cb: onChange },
-        click && { event: "click", cb: click },
-        ...(handleEvent && makeThatArray(handleEvent)),
-      ],
-      children: options.map(({ text, value }) => {
-        return { tag: option, text, attrs: { value } };
-      }),
-    },
-  ];
   createDOMElem({
     tag: div,
     parent,
